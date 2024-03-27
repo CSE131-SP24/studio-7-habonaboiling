90ec47d4941022e6ed0ce06bee05ce9e8b8bbb05
nourahabona <nourahabona@localhost>
2024-03-27 14:18:27 -0500

    dgfb

--- src/studio7/Die.java
+++ src/studio7/Die.java
@@ -1,5 +1,14 @@
 package studio7;
 
 public class Die {
+public int sideAmt; 
 
+public Die(int sideAmt) { 
+	this.sideAmt = sideAmt; 
+}
+
+public int dieRoll ()
+{ 
+	return (int) (Math.random()*sideAmt); 
+}
 }
--- src/studio7/Rectangle.java
+++ src/studio7/Rectangle.java
@@ -1,5 +1,19 @@
 package studio7;
 
 public class Rectangle {
+	public int width;
+	public int length;
+	
+	// consrctr
+	public Rectangle(int width, int length) {
+		this.width=width;
+		this.length=length;
+		
+	}
+	
+	//class mthds
+	public int area () {
+		return this.width*this.length; 
+	}
 
 }
